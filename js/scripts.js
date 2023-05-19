e079db56f3d1ac703be32cf0ac2055efe3654985
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-05-19 19:53:12 +0200

    add comments to js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 alert('Hello World');
 // a list of my favorite food items
 let favoriteFood = ['banana', 'apple cake', 'tuna salad', 'boiled rice with chicken', 'pancakes with cottage cheese'];
+//array methods to practice adding and deleting items from/to an array
 document.write(favoriteFood);
 favoriteFood.push('ice-cream');
 document.write('<br>' + favoriteFood);
