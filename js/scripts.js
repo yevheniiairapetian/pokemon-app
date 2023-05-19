8aac13d2fecd1e772cb15ef1f90f5a8e23f0fea8
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-05-19 19:51:27 +0200

    add structure files to the project, add practice js code

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// a pop-up window alerting a message to the browswer window
+alert('Hello World');
+// a list of my favorite food items
+let favoriteFood = ['banana', 'apple cake', 'tuna salad', 'boiled rice with chicken', 'pancakes with cottage cheese'];
+document.write(favoriteFood);
+favoriteFood.push('ice-cream');
+document.write('<br>' + favoriteFood);
+favoriteFood.shift();
+document.write('<br>' + favoriteFood);
