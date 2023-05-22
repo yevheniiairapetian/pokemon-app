4392b92d737fc64471b15bb43dac1343d328d891
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-05-22 18:45:33 +0200

    feat: add pokemon objects to pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+let pokemonList = [
+        {name: 'Bulbaaur', 
+        height: 0.7,
+        abilities: ['Chlorophyll', 'Overgrow'],
+        weight: 6.9,
+        catchRate: 0,
+        eggGroups:['Monster', 'Grass'],
+        genderRatio:{male:87.5, female:12.5},
+        hatchSteps: 5100        
+    },
+    {
+        name: 'Ivysaur', 
+        height: 1,
+        abilities: ['Chlorophyll', 'Overgrow'],
+        weight: 13,
+        catchRate: 0,
+        eggGroups:['Monster', 'Grass'],
+        genderRatio:{male:87.5, female:12.5},
+        hatchSteps: 5100    
+    },
+    {
+        name: 'Venusaur', 
+        height: 2,
+        abilities: ['Chlorophyll', 'Overgrow'],
+        weight: 100,
+        catchRate: 0,
+        eggGroups:['Monster', 'Grass'],
+        genderRatio:{male:87.5, female:12.5},
+        hatchSteps: 5100    
+    },
+    {
+        name: 'Charmander', 
+        height: 0.6,
+        abilities: ['Blaze', 'Solar-power'],
+        weight: 8.5,
+        catchRate: 0,
+        eggGroups:['Monster', 'Dragon'],
+        genderRatio:{male:87.5, female:12.5},
+        hatchSteps: 5100    
+    },
+    {
+        name: 'Charmeleon', 
+        height: 1.1,
+        abilities: ['Blaze', 'Solar-power'],
+        weight: 19,
+        catchRate: 0,
+        eggGroups:['Monster', 'Dragon'],
+        genderRatio:{male:87.5, female:12.5},
+        hatchSteps: 5100    
+    }
+];
