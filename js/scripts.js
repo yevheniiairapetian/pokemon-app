c4ea111f70194604a2298ccc9441d59663411c00
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-05-23 16:24:42 +0200

    feat: add basic styles to the main elements, logo

--- js/scripts.js
+++ js/scripts.js
@@ -61,10 +61,10 @@ let pokemonList = [
 // a for loop to iterate through the Pokemon list and print their values
 
 for(i = 0; i < pokemonList.length; i++){
-    document.write('<p>' + 'Pokemon\'s Name: '+ pokemonList[i].name + '</p>' + '<p>' + 'Height:' + pokemonList[i].height + 'm'+ '</p>' + '<p>' + 'Abilities:' + pokemonList[i].abilities + '</p>' + '<p>' + 'Weight:' + pokemonList[i].weight + 'kg' + '</p>' + '<p>' + 'Catch Rate:' + pokemonList[i].catchRate + '</p>' + '<p>' + 'Egg Groups:' + pokemonList[i].eggGroups + '</p>' + '<p>' + 'Gender Ratio:' + '<br>' + '<span>' + 'Male - ' + '</span>' + pokemonList[i].genderRatio.male + '<span> ' + 'Female - ' + '</span>' + pokemonList[i].genderRatio.female + '</p>' + '<p>' + 'Hatch Steps:' + pokemonList[i].hatchSteps + '</p>');
+    document.write('<div class="pokemon-items">' + '<p>' + 'Pokemon\'s Name: '+ pokemonList[i].name + '</p>' + '<p>' + 'Height:' + pokemonList[i].height + 'm'+ '</p>' + '<p>' + 'Abilities:' + pokemonList[i].abilities + '</p>' + '<p>' + 'Weight:' + pokemonList[i].weight + 'kg' + '</p>' + '<p>' + 'Catch Rate:' + pokemonList[i].catchRate + '</p>' + '<p>' + 'Egg Groups:' + pokemonList[i].eggGroups + '</p>' + '<p>' + 'Gender Ratio:' + '<br>' + '<p class="additional-info">' + 'Male - ' + '</p>' + pokemonList[i].genderRatio.male + '<p class="additional-info"> ' + 'Female - ' + '</p>' + pokemonList[i].genderRatio.female + '<p>' + 'Hatch Steps:' + pokemonList[i].hatchSteps + '</p>' + '</div>');
         if(pokemonList[i].height === 2 && pokemonList[i].name === 'Venusaur'){
-            document.write('WOW! That\'s a big height!' + '<br>');
+            document.write('<p class="additional-info">' + 'WOW! That\'s a big height!' + '</p>');
         }else if(pokemonList[i].height === 0.6 && pokemonList[i].name === 'Charmander'){
-            document.write('WOW! So tiny!' + '<br>');
+            document.write('<p class="additional-info">' + 'WOW! So tiny!' + '</p>');
         }
 }
