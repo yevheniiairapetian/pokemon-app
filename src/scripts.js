8383af67133a01e6ed4d9141430794554096bdee
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-06-17 15:17:58 +0200

    fix: fix spinner behavior

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,11 @@
 // created an array of objects: array is a list of pokemons, 
 // each object is a pokemon with respective properties. 
 //add the IIFE containing the pokemonList array
- window.onload = function () {
+$(document).ready( function()
+{
+  $('.sp').addClass('sp-none');
+  });
 
-// const myTimeout2 = setTimeout($('.sp').show(2000), 3000);    
-const myTimeout = setTimeout($('.sp').hide(), 5000); }
 
 
 
